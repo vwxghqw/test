389d01386eed50f737074411384bb2581ed4d742
vwxghqw <87975155+vwxghqw@users.noreply.github.com>
2023-01-22 21:04:16 +0800

    Add files via upload

---
+++ 白屏.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+#include<windows.h>
+using namespace std;
+int main()
+{
+	system("color f1");
+	keybd_event(VK_F11,0,0,0);
+	keybd_event(VK_F11,0,KEYEVENTF_KEYUP,0);
+	getchar();
+}
